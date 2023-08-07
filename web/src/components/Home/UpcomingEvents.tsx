74a14b442074d521160adad01cadd9f1906c368d
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-08-07 05:45:46 +0530

    Added Events

--- web/src/components/Home/UpcomingEvents.tsx
+++ web/src/components/Home/UpcomingEvents.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import EventCard from './EventCard'
+import EventCard from './EventCard';
+import { useEffect, useState } from 'react';
+import {EventI} from '@/models';
+// import { Event } from '../../models/event.type';
 
 const UpcomingEvents = () => {
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch(import.meta.env.VITE_EVENT_SOURCE);
+      const data = await response.json();
+      setEvents(data.slice(0, 3));
+    };
+    fetchEvents();
+  }, []);
+
+
   return (
     <>
     <div className='my-4'>
-        <h1 className="font-bold my-2 text-xl text-center">Upcoming Events</h1>
+        <h1 className="font-bold my-2 text-xl text-center">Events</h1>
         <div className='flex md:flex-row flex-col md:justify-between md:w-4/5 mx-auto justify-center'>
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
+            {events.map((event: EventI, index: number) => (
+                <EventCard key={index} event={event} />
+            ))}
         </div>
     </div>
     </>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.md']
-})
+export default () => {
+  return defineConfig({
+    plugins: [react()],
+    assetsInclude: ['**/*.md'],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  });
+};
